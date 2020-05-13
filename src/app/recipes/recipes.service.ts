88e49b3aab52f7cc5f37e0b7f83b1f3631027e61
J <jeff210372@icloud.com>
2020-05-13 13:24:55 +0800

    recipes.service.ts

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,61 +1,45 @@
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map, tap } from "rxjs/operators";
 
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
 
-@Injectable()
-export class RecipeService {
-  recipesChanged = new Subject<Recipe[]>();
+@Injectable({ providedIn: "root" })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
-  // private recipes: Recipe[] = [
-  //   new Recipe(
-  //     'Tasty Schnitzel',
-  //     'A super-tasty Schnitzel - just awesome!',
-  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-  //   ),
-  //   new Recipe(
-  //     'Big Fat Burger',
-  //     'What else you need to say?',
-  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-  //   )
-  // ];
-  private recipes: Recipe[] = [];
-
-  constructor(private slService: ShoppingListService) {}
-
-  setRecipes(recipes: Recipe[]) {
-    this.recipes = recipes;
-    this.recipesChanged.next(this.recipes.slice());
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        //'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
+        "https://cooking-7b1d0.firebaseio.com/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
-  getRecipes() {
-    return this.recipes.slice();
-  }
-
-  getRecipe(index: number) {
-    return this.recipes[index];
-  }
-
-  addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
-  }
-
-  addRecipe(recipe: Recipe) {
-    this.recipes.push(recipe);
-    this.recipesChanged.next(this.recipes.slice());
-  }
-
-  updateRecipe(index: number, newRecipe: Recipe) {
-    this.recipes[index] = newRecipe;
-    this.recipesChanged.next(this.recipes.slice());
-  }
-
-  deleteRecipe(index: number) {
-    this.recipes.splice(index, 1);
-    this.recipesChanged.next(this.recipes.slice());
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        //"https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json"
+        "https://cooking-7b1d0.firebaseio.com/recipes.json"
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
